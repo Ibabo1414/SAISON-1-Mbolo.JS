35d25c61990d46972cc21087634be75860a590f0
Ibabo <iyourimendoza@gmail.com>
2020-05-09 20:02:40 +0100

    correction d'un bug à l'épisode 13 de la saison 1 : Mbolo JS

--- Js/episode-13.js
+++ Js/episode-13.js
@@ -2,22 +2,15 @@
 /*Ici, il est question d'écrire un programme qui demande sans cesse à l'utilisateur de renseigner 
 la bonne réponse d’une opération (ex: Combien font 5+1 ?) 
 jusqu’à ce qu’il renseigne la bonne réponse.*/
-
-var reponse = parseInt(prompt("Combien font 10 + 12 ?"));
+var x = 550;
+var reponse = parseInt(prompt("Combien font 475 + 75 ?"));
 while(isNaN(reponse)){
 	reponse = parseInt(prompt("Vous vous êtes trompé... Veillez insérer uniquement des chiffres pour l'addition. Ex. 18"));
 	/*J'ai ajouté cette boucle pour évité que les 
-	que l'utilisateur insère des'string' au lieu de chiffres*/
+	que l'utilisateur insère des'string' au lieu des chiffres*/
 }
-var x = 22;
-
-while (reponse != 22){
-	alert("Mauvaise reponse !");
-	reponse = parseInt(prompt("Vous avez insérez une mauvaise reponse... Combien font 10 + 12 ?")); //Ce champ s'affiche lorsque la reponse est fausse.
-	alert("Vous avez encore donné une fausse reponse... !");
-	reponse = parseInt(prompt("Vous avez une nouvelle fois insérez une mauvaise reponse... Combien font 10 + 12 ?")); //Ce champ s'affiche lorsque la reponse est fausse.
- 	break; //Break a été utilisé ici pour éviter que la boucle ne se repete à l'infini.
+while (reponse !== x){
+	alert("Mauvaise reponse !");//alert s'affichera uniquement lorsque la reponse sera fausse.
+	reponse = parseInt(prompt("Vous avez insérez une mauvaise reponse... Ne vous découragez pas ! Combien font 475 + 75 ?"));//Ce champ s'affiche lorsque la reponse est fausse.
 }
-if(reponse === 22){
- 	alert("Félicitation, vous avez trouver la bonne reponse.");
-}
+alert("Félicitation, vous avez trouver la bonne reponse."); //alert ne va afficher ce champ que la reponse sera correcte.
